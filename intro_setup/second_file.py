8ae7bda0ca2d0bb3cae2edc06ee94175dd549534
DINESHPANDIAN-J <datadinesh1@gmail.com>
2025-05-07 13:58:59 +0530

    01_intro_setup folder name changed to intro_setup

--- intro_setup/second_file.py
+++ intro_setup/second_file.py
@@ -6,6 +6,6 @@ app = FastAPI()
 def read_home():
     return {"message": "vakam da mapla home la irunthu!"}
 
-app.get("/about") # defining a path operation for about endpoint
+@app.get("/about") # defining a path operation for about endpoint
 def read_about():
     return {"message_1": "vakam da mapla about la irunthu!", "message_2": "Nan tha Dinesh J!"}
